7f53a55f0fb68c49034240d179d59960431c47ea
Nic Dalmasso <ndalmass@andrew.cmu.edu>
2020-02-24 15:49:21 -0500

    Removing files after change in folder structure

--- qr_algorithms/small_list.py
+++
@@ -1,6 +0,0 @@
-classifier_cde_dict = {
-    'xgb_d3_n100': ('xgb', {'max_depth': 3, 'n_estimators': 100}),
-    'xgb_d5_n250': ('xgb', {'max_depth': 5, 'n_estimators': 250}),
-    'RF100': ('rf', {'n_estimators': 100}),
-    'pytorch': ('pytorch', {'epochs': 10, 'batch_size': 50}, {'neur_shapes': (64, 64)})
-}
