8c00ed0334216ec12b7857c9e5199e1182480983
Nic Dalmasso <ndalmass@andrew.cmu.edu>
2020-02-24 15:29:49 -0500

    Adding files to run ACORE in its entirety

---
+++ acore.py
@@ -0,0 +1,237 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import argparse
+import pickle
+import sys
+sys.path.append("..")
+
+from tqdm.auto import tqdm
+from datetime import datetime
+from or_classifiers.complete_list import classifier_dict, classifier_conv_dict
+from qr_algorithms.complete_list import classifier_cde_dict
+from models.sen_poisson import SenPoissonLoader
+from models.camelus_wl import CamelusSimLoader
+from utils.qr_functions import train_qr_algo
+from utils.functions import train_clf, compute_statistics_single_t0
+
+
+model_dict = {
+    'camelus': CamelusSimLoader,
+    'poisson': SenPoissonLoader
+}
+
+
+def main(b, b_prime, alpha, classifier, class_cde, sample_size_obs, run, t_star, c_star,
+         debug=False, seed=7, n_sampled=500, size_reference=1000):
+
+    # Setup the variables, also to account for debug runs
+    np.random.seed(seed)
+    b = b if not debug else 100
+    b_prime = b_prime if not debug else 100
+    sample_size_obs = sample_size_obs if not debug else 1
+    n_sampled = n_sampled if not debug else 10
+
+    # Create the loader object, which drives most
+    print('----- Loading Simulations In')
+    model_obj = model_dict[run]() if not debug else model_dict[run](num_grid=21)
+    t0_val = model_obj.true_t0
+
+    # Also, calculate the reference distribution
+    model_obj.set_reference_g(size_reference=size_reference)
+
+    # Get the correct functions
+    msnh_sampling_func = model_obj.sample_msnh_algo5
+    grid_param = model_obj.grid
+    clf_model = classifier_dict[classifier]
+    gen_sample_func = model_obj.generate_sample
+    t0_grid = model_obj.grid
+    gen_obs_func = model_obj.sample_sim
+    classifier = classifier.replace('\n', '').replace(' ', '-')
+
+    # Create a sample of observed data that are going to be used later
+    # and compute statistics tau value for each t0
+    x_obs = gen_obs_func(sample_size=sample_size_obs, true_param=t0_val)
+
+    # Calculate Odds
+    print('----- Calculating Odds')
+    if t_star:
+        pbar = tqdm(total=t0_grid.shape[0], desc=r'Calculating True $\tau$')
+        tau_obs = []
+        for t0 in t0_grid:
+            tau_obs.append(model_obj.compute_exact_tau(x_obs=x_obs, t0_val=t0, meshgrid=grid_param))
+            pbar.update(1)
+        tau_obs = np.array(tau_obs)
+    else:
+        # Compute Odds via classifier
+        clf = train_clf(sample_size=b, clf_model=clf_model, gen_function=gen_sample_func, d=model_obj.d,
+                        clf_name=classifier)
+        print('----- %s Trained' % classifier)
+
+        pbar = tqdm(total=len(t0_grid), desc='Calculate Odds')
+        tau_obs = []
+        for theta_0 in t0_grid:
+            tau_obs.append(compute_statistics_single_t0(
+                clf=clf, obs_sample=x_obs, t0=theta_0, d=model_obj.d, d_obs=model_obj.d_obs, grid_param_t1=grid_param))
+            pbar.update(1)
+        tau_obs = np.array(tau_obs)
+
+    # Train Quantile Regression
+    if c_star:
+        pbar = tqdm(total=t0_grid.shape[0], desc=r'Calculating Distribution True $\tau$')
+        tau_distr = []
+        for t0 in t0_grid:
+            tau_distr.append(model_obj.compute_exact_tau_distr(
+                t0_val=t0,
+                meshgrid=grid_param,
+                n_sampled=n_sampled,
+                sample_size_obs=sample_size_obs))
+            pbar.update(1)
+
+        tau_distr = np.array(tau_distr)
+        np.save(file='sims/%stau_distr_t0_%s_%s_%ssampled_%ssamplesizeobs.npy' % (
+            model_obj.out_directory, b, b_prime, n_sampled, sample_size_obs
+        ), arr=tau_distr)
+        t0_pred_vec = np.quantile(a=tau_distr, q=alpha, axis=1)
+
+    else:
+        print('----- Training Quantile Regression Algorithm')
+        theta_mat, sample_mat = msnh_sampling_func(b_prime=b_prime, sample_size=sample_size_obs)
+
+        # Compute the tau values for QR training
+        if t_star:
+            stats_mat = np.array([model_obj.compute_exact_tau(
+                    x_obs=sample_mat[kk, :, :], t0_val=theta_0,
+                    meshgrid=grid_param) for kk, theta_0 in enumerate(theta_mat)])
+        else:
+            stats_mat = np.array([compute_statistics_single_t0(
+                clf=clf, d=model_obj.d, d_obs=model_obj.d_obs, grid_param_t1=grid_param,
+                t0=theta_0, obs_sample=sample_mat[kk, :, :]) for kk, theta_0 in enumerate(theta_mat)])
+
+        clf_params = classifier_cde_dict[class_cde]
+
+        t0_pred_vec = train_qr_algo(model_obj=model_obj, alpha=alpha, theta_mat=theta_mat, stats_mat=stats_mat,
+                                    algo_name=clf_params[0], learner_kwargs=clf_params[1],
+                                    pytorch_kwargs=clf_params[2] if len(clf_params) > 2 else None,
+                                    prediction_grid=t0_grid)
+
+    # Confidence Region
+    print('----- Creating Confidence Region')
+    simultaneous_nh_decision = []
+    for jj, t0_pred in enumerate(t0_pred_vec):
+        simultaneous_nh_decision.append([t0_pred, tau_obs[jj], int(tau_obs[jj] < t0_pred)])
+
+    # Saving data
+    print('----- Saving Data')
+    save_dict = {
+        'background': t0_grid[:, 0],
+        'signal': t0_grid[:, 1],
+        'tau_statistics': tau_obs,
+        'simul_nh_cutoff': [el[0] for el in simultaneous_nh_decision],
+        'simul_nh_decision': [el[2] for el in simultaneous_nh_decision],
+        'b': b,
+        'b_prime': b_prime,
+        'seed': seed,
+        'sample_size_obs': sample_size_obs,
+        'classifier': classifier,
+        't_star': t_star
+    }
+    outfile_name = '2d_confint_%s_data_b_%s_bprime_%s_%s_%s_n%s_%s_%s_%s_%s%s_%s.pkl' % (
+        run, b, b_prime, t0_val[0], t0_val[1], sample_size_obs, classifier, class_cde, n_sampled,
+        '' if not t_star else '_taustar',
+        '' if not c_star else '_cstar',
+        datetime.strftime(datetime.today(), '%Y-%m-%d')
+    )
+    outdir = 'sims/%s' % model_obj.out_directory
+    pickle.dump(obj=save_dict, file=open(outdir + outfile_name, 'wb'))
+
+    # Visualization
+    plot_df = pd.DataFrame.from_dict({
+        'background': t0_grid[:, 0],
+        'signal': t0_grid[:, 1],
+        'tau_statistics': tau_obs,
+        'simul_nh_cutoff': [el[0] for el in simultaneous_nh_decision],
+        'simul_nh_decision': [el[2] for el in simultaneous_nh_decision]
+    })
+
+    col_vec = ['blue']
+    alpha_vec = [0.75, 0.1]
+    theta_0_plot = plot_df['background'].values
+    theta_1_plot = plot_df['signal'].values
+
+    plt.figure(figsize=(12, 8))
+    for ii, col in enumerate(['simul_nh_decision']):
+        value_temp = plot_df[col].values
+        marker = np.array(["x" if el else "o" for el in value_temp])
+        unique_markers = set(marker)
+
+        for j, um in enumerate(unique_markers):
+            mask = marker == um
+            plt.scatter(x=theta_0_plot[mask], y=theta_1_plot[mask],
+                        marker=um, color=col_vec[ii], alpha=alpha_vec[j])
+
+        plt.scatter(x=t0_val[0], y=t0_val[1], color='r', marker='*', s=500)
+        plt.xlabel('Background', fontsize=25)
+        plt.ylabel('Signal', fontsize=25)
+        plt.xticks(fontsize=20)
+        plt.yticks(fontsize=20)
+        plt.title("2D Confidence Interval, %s Example, B=%s, B'=%s, n=%s%s%s" % (
+            run.title(), b, b_prime, sample_size_obs,
+            '' if not t_star else '\n tau_star',
+            '' if not c_star else ', c_star'), fontsize=25)
+
+    plt.tight_layout()
+    image_name = '2d_confint_%s_b_%s_bprime_%s_%s_%s_%s_n%s%s%s_%s.pdf' % (
+        run, b, b_prime, t0_val[0], t0_val[1], sample_size_obs, classifier,
+        '' if not t_star else '_taustar',
+        '' if not c_star else '_cstar',
+        datetime.strftime(datetime.today(), '%Y-%m-%d'))
+    plt.savefig('images/%s/' % model_obj.out_directory + image_name)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', action="store", type=int, default=7,
+                        help='Random State')
+    parser.add_argument('--b', action="store", type=int, default=50000,
+                        help='Sample size to train the classifier for calculating odds')
+    parser.add_argument('--b_prime', action="store", type=int, default=5000,
+                        help='Sample size to train the quantile regression algorithm')
+    parser.add_argument('--alpha', action="store", type=float, default=0.1,
+                        help='Statistical confidence level')
+    parser.add_argument('--debug', action='store_true', default=False,
+                        help='If true, a very small value for the sample sizes is fit to make sure the'
+                             'file can run quickly for debugging purposes')
+    parser.add_argument('--t_star', action='store_true', default=False,
+                        help='If true, the odds are computed exactly.')
+    parser.add_argument('--c_star', action='store_true', default=False,
+                        help='If true, the QR rejection is computed via MC.')
+    parser.add_argument('--sample_size_obs', action="store", type=int, default=10,
+                        help='Sample size of the actual observed data.')
+    parser.add_argument('--run', action="store", type=str, default='poisson',
+                        help='Problem to run')
+    parser.add_argument('--classifier', action="store", type=str, default='qda',
+                        help='Classifier to run for learning the odds')
+    parser.add_argument('--class_cde', action="store", type=str, default='xgb_d3_n100',
+                        help='Classifier to run for QR')
+    parser.add_argument('--n_sampled', action="store", type=int, default=500,
+                        help='Number of values to sample for c_star calculation.')
+    parser.add_argument('--size_reference', action="store", type=int, default=1000,
+                        help='Number of samples used for the reference distribution')
+    argument_parsed = parser.parse_args()
+
+    main(
+        b=argument_parsed.b,
+        b_prime=argument_parsed.b_prime,
+        alpha=argument_parsed.alpha,
+        debug=argument_parsed.debug,
+        sample_size_obs=argument_parsed.sample_size_obs,
+        seed=argument_parsed.seed,
+        classifier=classifier_conv_dict[argument_parsed.classifier],
+        run=argument_parsed.run,
+        t_star=argument_parsed.t_star,
+        c_star=argument_parsed.c_star,
+        class_cde=argument_parsed.class_cde,
+        n_sampled=argument_parsed.n_sampled,
+        size_reference=argument_parsed.size_reference
+    )
---
+++ b_prime_analysis.py
@@ -0,0 +1,180 @@
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import argparse
+import sys
+sys.path.append("..")
+
+from tqdm.auto import tqdm
+from datetime import datetime
+from xgboost import XGBClassifier
+from utils.functions import Suppressor, train_clf, compute_statistics_single_t0
+from models.camelus_wl import CamelusSimLoader
+from models.sen_poisson import SenPoissonLoader
+from or_classifiers.complete_list import classifier_dict, classifier_conv_dict
+from qr_algorithms.complete_list import classifier_cde_dict as classifier_cde_dict_full
+from qr_algorithms.small_list import classifier_cde_dict as classifier_cde_dict_small
+from utils.qr_functions import train_qr_algo
+
+
+model_dict = {
+    'camelus': CamelusSimLoader,
+    'poisson': SenPoissonLoader
+}
+
+
+def main(b, alpha, classifier, sample_size_obs, run, n_eval_grid=101,
+         debug=False, seed=7, sample_size_check=1000, size_reference=1000):
+
+    # Setup the variables, also to account for debug runs
+    np.random.seed(seed)
+    b = b if not debug else 100
+    sample_size_obs = sample_size_obs if not debug else 5
+    classifier_cde_dict = classifier_cde_dict_full if not debug else classifier_cde_dict_small
+
+    # Create the loader object, which drives most
+    print('----- Loading Simulations In')
+    model_obj = model_dict[run]()
+
+    # Also, calculate the reference distribution
+    model_obj.set_reference_g(size_reference=size_reference)
+
+    # Get the correct functions
+    msnh_sampling_func = model_obj.sample_msnh_algo5
+    grid_param = model_obj.grid
+    clf_model = classifier_dict[classifier]
+    gen_sample_func = model_obj.generate_sample
+    classifier = classifier.replace('\n', '').replace(' ', '-')
+
+    # Then generate first the thetas used for checking coverage
+    theta_vec, x_vec = model_obj.sample_sim_check(sample_size=sample_size_check, n=sample_size_obs)
+
+    # Compute Odds via classifier
+    print('----- Calculating Odds')
+    clf = train_clf(sample_size=b, clf_model=clf_model, gen_function=gen_sample_func,
+                    d=model_obj.d, clf_name=classifier)
+    tau_obs = np.array([
+        compute_statistics_single_t0(
+            clf=clf, obs_sample=x_vec[kk, :, :].reshape(-1, model_obj.d_obs), d=model_obj.d, d_obs=model_obj.d_obs,
+            t0=theta_0, grid_param_t1=grid_param) for kk, theta_0 in enumerate(theta_vec)])
+    print('----- %s Trained' % classifier)
+
+    # Loop over B'
+    b_prime_vec = model_obj.b_prime_vec if not debug else [500, 1000]
+    out_val = []
+    out_cols = ['b_prime', 'classifier', 'class_cde', 'run', 'n_eval_grid', 'sample_check',
+                'sample_reference', 'percent_correct_coverage', 'average_coverage',
+                'percent_correct_coverage_lr', 'average_coverage_lr',
+                'percent_correct_coverage_1std', 'average_coverage_1std',
+                'percent_correct_coverage_2std', 'average_coverage_2std']
+    for b_prime in np.array(b_prime_vec).astype(int):
+        # First generate the samples to train b_prime algorithm
+        np.random.seed(seed)
+        theta_mat, sample_mat = msnh_sampling_func(b_prime=b_prime, sample_size=sample_size_obs)
+        stats_mat = np.array([compute_statistics_single_t0(
+                clf=clf, d=model_obj.d, d_obs=model_obj.d_obs, grid_param_t1=grid_param,
+                t0=theta_0, obs_sample=sample_mat[kk, :, :]) for kk, theta_0 in enumerate(theta_mat)])
+
+        pbar = tqdm(total=len(classifier_cde_dict.keys()),
+                    desc=r'Working on QR classifiers, b=%s' % b_prime)
+        for clf_name_qr, clf_params in sorted(classifier_cde_dict.items(), key=lambda x: x[0]):
+
+            if b_prime > 10000 and 'RF' in clf_name_qr:
+                continue
+
+            t0_pred_vec = train_qr_algo(model_obj=model_obj, theta_mat=theta_mat, stats_mat=stats_mat,
+                                        algo_name=clf_params[0], learner_kwargs=clf_params[1],
+                                        pytorch_kwargs=clf_params[2] if len(clf_params) > 2 else None,
+                                        alpha=alpha, prediction_grid=theta_vec)
+
+            in_vec = np.array([
+                int(tau_obs[jj] > t0_pred_vec[jj]) for jj in range(theta_vec.shape[0])
+            ])
+
+            # Calculate the mean
+            model = XGBClassifier(depth=3, n_estimators=100)
+            model.fit(theta_vec.reshape(-1, model_obj.d), in_vec.reshape(-1,))
+            pred_grid = model_obj.pred_grid
+            pred_cov_mean = model.predict_proba(pred_grid)[:, 1]
+            percent_correct_coverage = np.average((pred_cov_mean > (1.0 - alpha)).astype(int))
+            average_coverage = np.average(pred_cov_mean)
+
+            # Calculate the upper limit
+            x = theta_vec.reshape(-1, 2)
+            y = in_vec.reshape(-1,)
+            # estimate the model
+            X = sm.add_constant(x)
+
+            with Suppressor():
+                model = sm.Logit(y, X).fit(full_output=False)
+            proba = model.predict(X)
+
+            percent_correct_coverage_lr = np.average((proba > (1.0 - alpha)).astype(int))
+            average_coverage_lr = np.average(proba)
+
+            # estimate confidence interval for predicted probabilities
+            cov = model.cov_params()
+            gradient = (proba * (1 - proba) * X.T).T  # matrix of gradients for each observation
+            std_errors = np.array([np.sqrt(np.dot(np.dot(g, cov), g)) for g in gradient])
+            c = 1  # multiplier for confidence interval
+            upper = np.maximum(0, np.minimum(1, proba + std_errors * c))
+            percent_correct_coverage_upper = np.average((upper > (1.0 - alpha)).astype(int))
+            average_coverage_upper = np.average(upper)
+
+            upper_2std = np.maximum(0, np.minimum(1, proba + std_errors * 1.96))
+            percent_correct_coverage_upper_2std = np.average((upper_2std > (1.0 - alpha)).astype(int))
+            average_coverage_upper_2std = np.average(upper_2std)
+
+            out_val.append([
+                b_prime, classifier, clf_name_qr, run, n_eval_grid, sample_size_check, size_reference,
+                percent_correct_coverage, average_coverage, percent_correct_coverage_lr, average_coverage_lr,
+                percent_correct_coverage_upper, average_coverage_upper,
+                percent_correct_coverage_upper_2std, average_coverage_upper_2std
+            ])
+
+            pbar.update(1)
+
+    # Saving the results
+    out_df = pd.DataFrame.from_records(data=out_val, index=range(len(out_val)), columns=out_cols)
+    out_dir = 'sims/%s' % model_obj.out_directory
+    out_filename = 'b_prime_analysis_%s_%s_alpha%s_ngrid%s_sizecheck%s_bprimemax%s_logregint_%s.csv' % (
+        classifier, run, str(alpha).replace('.', '-'),
+        n_eval_grid, sample_size_check, np.max(b_prime_vec),
+        datetime.strftime(datetime.today(), '%Y-%m-%d')
+    )
+    out_df.to_csv(out_dir + out_filename)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', action="store", type=int, default=7,
+                        help='Random State')
+    parser.add_argument('--b', action="store", type=int, default=50000,
+                        help='Sample size to train the classifier for calculating odds')
+    parser.add_argument('--alpha', action="store", type=float, default=0.1,
+                        help='Statistical confidence level')
+    parser.add_argument('--debug', action='store_true', default=False,
+                        help='If true, a very small value for the sample sizes is fit to make sure the'
+                             'file can run quickly for debugging purposes')
+    parser.add_argument('--verbose', action='store_true', default=False,
+                        help='If true, logs are printed to the terminal')
+    parser.add_argument('--sample_size_obs', action="store", type=int, default=10,
+                        help='Sample size of the actual observed data.')
+    parser.add_argument('--run', action="store", type=str, default='poisson',
+                        help='Problem to run')
+    parser.add_argument('--classifier', action="store", type=str, default='qda',
+                        help='Classifier to run for learning the odds')
+    parser.add_argument('--size_reference', action="store", type=int, default=1000,
+                        help='Number of samples used for the reference distribution')
+    argument_parsed = parser.parse_args()
+
+    main(
+        b=argument_parsed.b,
+        alpha=argument_parsed.alpha,
+        debug=argument_parsed.debug,
+        sample_size_obs=argument_parsed.sample_size_obs,
+        seed=argument_parsed.seed,
+        classifier=classifier_conv_dict[argument_parsed.classifier],
+        run=argument_parsed.run,
+        size_reference=argument_parsed.size_reference
+    )
