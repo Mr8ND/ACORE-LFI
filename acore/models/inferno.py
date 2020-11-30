e4e2691194c10eb8d3a350e4f6d76c0619461a9e
Mr8ND <niccolo.dalmasso@gmail.com>
2020-11-30 18:00:57 -0500

    Committing final version of INFERNO with total re-haul of nuisance parameters.

--- acore/models/inferno.py
+++ acore/models/inferno.py
@@ -3,17 +3,21 @@ import math
 import sys
 sys.path.append('..')
 
-from utils.functions import tensor_4d_mesh
+from functools import partial
+from utils.functions import tensor_4d_mesh, train_clf, mode_rows
 from scipy.stats import multivariate_normal, poisson, rv_discrete, expon
 from scipy.linalg import sqrtm
 from scipy.optimize import Bounds, minimize
+from scipy.stats import mode
+from xgboost import XGBRFClassifier
 
 
 class InfernoToyLoader:
 
     def __init__(self, s_param=50, r_param=0.0, lambda_param=3.0, b_param=1000, benchmark=None,
-                 s_low=0, s_high=100, r_low=-5, r_high=5, lambda_low=0, lambda_high=10, b_low=700, b_high=1300,
-                 out_dir='inferno_toy/', num_grid=21, num_pred_grid=101, preset_grid_n=21, *args, **kwargs):
+                 nuisance_parameters=True, s_low=0, s_high=100, r_low=-5, r_high=5, lambda_low=0, lambda_high=10,
+                 b_low=700, b_high=1300, out_dir='inferno_toy/', num_acore_grid=21, num_pred_grid=21,
+                 *args, **kwargs):
 
         self.true_s = s_param
         self.s_low = s_low
@@ -86,6 +90,7 @@ class InfernoToyLoader:
             self.b_low = b_low
             self.b_high = b_high
 
+        self.true_param = np.array([self.true_s, self.true_r, self.true_lambda, self.true_b])
         active_params_tuple = [True, False, False, False]
         low_bounds, high_bounds = [], []
         for idx, (low, high) in enumerate([(self.r_low, self.r_high), (self.lambda_low, self.lambda_high),
@@ -94,19 +99,42 @@ class InfernoToyLoader:
                 active_params_tuple[idx + 1] = True
                 low_bounds.append(low)
                 high_bounds.append(high)
+        self.bounds_opt = Bounds(low_bounds, high_bounds)
 
-        self.active_params = sum(active_params_tuple)
-        self.nuisance_parameters = self.active_params > 1
-        self.active_params_columns = np.where(active_params_tuple)[0]
-        self.nuisance_parameters_cols = np.where(active_params_tuple)[0][1:] if self.nuisance_parameters else None
+        self.d = sum(active_params_tuple)
+        self.active_params = self.d
+        self.active_params_cols = np.where(active_params_tuple)[0]
+        self.target_params_cols = [0] # target parameter is always the signal
 
-        print(self.active_params)
-        print(self.active_params_columns)
-        print(self.nuisance_parameters)
-        print(self.nuisance_parameters_cols)
-        print(low_bounds)
-        print(high_bounds)
-        print(stop)
+        # If nuisance parameters are treated as such, then determine which columns are nuisance parameters and
+        # which are not
+        self.nuisance_flag = self.d > 1 and nuisance_parameters
+        self.nuisance_parameters_cols = np.where(active_params_tuple)[0][1:] if self.nuisance_flag else None
+
+        # Prediction grids have to be mindful of how many parameters are active
+        self.num_pred_grid = num_pred_grid
+        self.num_acore_grid = num_acore_grid
+
+        # For the ACORE grid, it really depends on whether we consider nuisance parameters or not
+        if self.nuisance_flag:
+            self.pred_grid = np.linspace(start=self.s_low, stop=self.s_high, num=self.num_pred_grid)
+            self.idx_row_true_param = np.where((self.pred_grid == self.true_param[0]))[0][0]
+        else:
+            self.pred_grid = np.unique(
+                tensor_4d_mesh(np.meshgrid(np.linspace(start=self.s_low, stop=self.s_high, num=self.num_pred_grid),
+                                           np.linspace(start=self.r_low, stop=self.r_high, num=self.num_pred_grid),
+                                           np.linspace(start=self.lambda_low, stop=self.lambda_high,
+                                                       num=self.num_pred_grid),
+                                           np.linspace(start=self.b_low, stop=self.b_high, num=self.num_pred_grid))),
+                axis=0)[:, self.active_params_cols]
+            self.idx_row_true_param = np.where((self.pred_grid == self.true_param[:self.d]).all(axis=1))[0][0]
+        self.acore_grid = np.unique(
+            tensor_4d_mesh(np.meshgrid(np.linspace(start=self.s_low, stop=self.s_high, num=self.num_pred_grid),
+                                       np.linspace(start=self.r_low, stop=self.r_high, num=self.num_pred_grid),
+                                       np.linspace(start=self.lambda_low, stop=self.lambda_high,
+                                                   num=self.num_pred_grid),
+                                       np.linspace(start=self.b_low, stop=self.b_high, num=self.num_acore_grid))),
+            axis=0)[:, self.active_params_cols]
 
         self.regen_flag = False
         self.out_directory = out_dir
@@ -117,36 +145,8 @@ class InfernoToyLoader:
 
         self.b_sample_vec = [50, 100, 500, 1000, 5000, 10000, 50000, 100000, 200000]
         self.b_prime_vec = [100, 500, 1000, 5000, 10000, 50000, 100000]
-        self.d = 4
         self.d_obs = 3
-
-        self.num_grid = num_grid if self.active_params <= 2 else preset_grid_n
-        self.grid = np.unique(
-            tensor_4d_mesh(np.meshgrid(np.linspace(start=self.s_low, stop=self.s_high, num=self.num_grid),
-                           np.linspace(start=self.r_low, stop=self.r_high, num=self.num_grid),
-                           np.linspace(start=self.lambda_low, stop=self.lambda_high,
-                                       num=self.num_grid),
-                           np.linspace(start=self.b_low, stop=self.b_high, num=self.num_grid))), axis=0)
-
-        self.num_pred_grid = num_pred_grid if self.active_params <= 2 else preset_grid_n
-        self.pred_grid = np.unique(
-            tensor_4d_mesh(np.meshgrid(np.linspace(start=self.s_low, stop=self.s_high, num=self.num_grid),
-                                       np.linspace(start=self.r_low, stop=self.r_high, num=self.num_grid),
-                                       np.linspace(start=self.lambda_low, stop=self.lambda_high,
-                                                   num=self.num_grid),
-                                       np.linspace(start=self.b_low, stop=self.b_high, num=self.num_grid))), axis=0)
-        self.true_t0 = [self.true_s, self.true_r, self.true_lambda, self.true_b]
-
-    def select_active_parameters(self, param_mat):
-        return param_mat[:, self.active_params_columns]
-
-    def _nuisance_parameter_func(self, nu_params, x_obs, target_params, fixed_params, clf_odds):
-        param_mat = np.hstack((
-            np.tile(np.concatenate((target_params, nu_params, fixed_params)), x_obs.shape[0]).reshape(-1, self.d),
-            x_obs.reshape(-1, self.d_obs)
-        ))
-        pred_mat = clf_odds.predict_proba(param_mat)
-        return -1 * (np.prod(pred_mat[:, 1] / pred_mat[:, 0]))
+        self.nuisance_param_val = None
 
     @staticmethod
     def _compute_mean_vec(r_param):
@@ -163,13 +163,30 @@ class InfernoToyLoader:
         self.g_distribution = multivariate_normal(mean=self.mean_instrumental, cov=self.cov_instrumental)
 
     def sample_param_values(self, sample_size):
-        signal_vec = np.random.uniform(low=self.s_low, high=self.s_high, size=sample_size).reshape(-1, 1)
-        r_vec = np.random.uniform(low=self.r_low, high=self.r_high, size=sample_size).reshape(-1, 1)
-        lambda_vec = np.random.uniform(low=self.lambda_low, high=self.lambda_high, size=sample_size).reshape(-1, 1)
-        background_vec = np.random.uniform(low=self.b_low, high=self.b_high, size=sample_size).reshape(-1, 1)
-        return np.hstack((signal_vec, r_vec, lambda_vec, background_vec))
+        full_mat = np.random.uniform(low=self.s_low, high=self.s_high, size=sample_size).reshape(-1, 1)
+        if 1 in self.active_params_cols:
+            r_vec = np.random.uniform(low=self.r_low, high=self.r_high, size=sample_size).reshape(-1, 1)
+            full_mat = np.hstack((full_mat, r_vec))
+        if 2 in self.active_params_cols:
+            lambda_vec = np.random.uniform(low=self.lambda_low, high=self.lambda_high, size=sample_size).reshape(-1, 1)
+            full_mat = np.hstack((full_mat, lambda_vec))
+        if 3 in self.active_params_cols:
+            background_vec = np.random.uniform(low=self.b_low, high=self.b_high, size=sample_size).reshape(-1, 1)
+            full_mat = np.hstack((full_mat, background_vec))
+        return full_mat
+
+    def _create_complete_param_vec(self, true_param):
+        if 1 not in self.active_params_cols:
+            true_param = np.concatenate((true_param, np.array([self.true_r])))
+        if 2 not in self.active_params_cols:
+            true_param = np.concatenate((true_param, np.array([self.true_lambda])))
+        if 3 not in self.active_params_cols:
+            true_param = np.concatenate((true_param, np.array([self.true_b])))
+        return true_param
 
     def sample_sim(self, sample_size, true_param):
+        if len(true_param) < 4:
+            true_param = self._create_complete_param_vec(true_param)
         mixing_param = true_param[3] / (true_param[0] + true_param[3])
         cluster = np.random.binomial(n=1, p=mixing_param, size=sample_size)
 
@@ -247,9 +264,12 @@ class InfernoToyLoader:
 
         return mixing_param * first_term * first_term_exp + (1 - mixing_param) * second_term * second_term_exp
 
-    def compute_exact_prob(self, theta_mat, x_mat, p=0.5):
-        x_mat = x_mat.reshape(-1, self.d_obs)
-        theta_mat = theta_mat.reshape(-1, self.d)
+    def compute_exact_prob(self, theta_vec, x_vec, p=0.5):
+        x_mat = x_vec.reshape(-1, self.d_obs)
+        theta_mat = theta_vec.reshape(-1, self.d)
+        if theta_mat.shape[1] < 4:
+            theta_mat = np.apply_along_axis(arr=theta_mat.reshape(-1, self.d), axis=1,
+                                            func1d=lambda row: self._create_complete_param_vec(row))
 
         f_val = np.array([
             self._mixture_likelihood_manual(
@@ -262,9 +282,13 @@ class InfernoToyLoader:
         g_val = self.g_distribution.pdf(x=x_mat).reshape(-1, )
         return (f_val * p) / (f_val * p + g_val * (1 - p))
 
-    def compute_exact_odds(self, theta_mat, x_mat, p=0.5):
-        x_mat = x_mat.reshape(-1, self.d_obs)
-        theta_mat = theta_mat.reshape(-1, self.d)
+    def compute_exact_odds(self, theta_vec, x_vec, p=0.5):
+        x_mat = x_vec.reshape(-1, self.d_obs)
+        theta_mat = theta_vec.reshape(-1, self.d)
+
+        if theta_mat.shape[1] < 4:
+            theta_mat = np.apply_along_axis(arr=theta_mat.reshape(-1, self.d), axis=1,
+                                            func1d=lambda row: self._create_complete_param_vec(row))
 
         f_val = np.array([
             self._mixture_likelihood_manual(
@@ -278,6 +302,12 @@ class InfernoToyLoader:
         return (f_val * p) / (g_val * (1 - p))
 
     def compute_exact_or(self, t0, t1, x_obs):
+
+        if len(t0) < 4:
+            t0 = self._create_complete_param_vec(t0)
+        if len(t1) < 4:
+            t1 = self._create_complete_param_vec(t1)
+
         numerator = np.array([
             self._mixture_likelihood_manual(
                 x=x, mu_vec=self._compute_mean_vec(t0[1]),
@@ -297,45 +327,75 @@ class InfernoToyLoader:
         # Avoid straight up product for underflow
         return np.exp(np.sum(np.log(numerator)) - np.sum(np.log(denominator)))
 
-    def compute_exact_tau(self, x_obs, t0_val, meshgrid):
-        return np.min(np.array([np.sum(np.log(self.compute_exact_or(
-            x_obs=x_obs, t0=t0_val, t1=t1))) for t1 in meshgrid]))
+    # def compute_exact_tau(self, x_obs, t0_val, meshgrid):
+    #     return np.min(np.array([np.sum(np.log(self.compute_exact_or(
+    #         x_obs=x_obs, t0=t0_val, t1=t1))) for t1 in meshgrid]))
+    #
+    # def compute_exact_tau_distr(self, t0_val, meshgrid, n_sampled, sample_size_obs):
+    #     full_obs_sample = self.sample_sim(sample_size=n_sampled * sample_size_obs, true_param=t0_val)
+    #     sample_mat = full_obs_sample.reshape((n_sampled, sample_size_obs, self.d_obs))
+    #
+    #     tau_sample = np.array([self.compute_exact_tau(
+    #         x_obs=sample_mat[kk, :, :], t0_val=t0_val, meshgrid=meshgrid) for kk in range(sample_mat.shape[0])])
+    #
+    #     return tau_sample
 
-    def compute_exact_tau_distr(self, t0_val, meshgrid, n_sampled, sample_size_obs):
-        full_obs_sample = self.sample_sim(sample_size=n_sampled * sample_size_obs, true_param=t0_val)
-        sample_mat = full_obs_sample.reshape((n_sampled, sample_size_obs, self.d_obs))
+    def _nuisance_parameter_func(self, nu_params, x_obs, target_params, clf_odds):
+        param_mat = np.hstack((
+            np.tile(np.concatenate((target_params.reshape(-1,), nu_params.reshape(-1,))),
+                    x_obs.shape[0]).reshape(-1, self.d),
+            x_obs.reshape(-1, self.d_obs)
+        ))
+        pred_mat = clf_odds.predict_proba(param_mat)
+        return -1 * (np.prod(pred_mat[:, 1] / pred_mat[:, 0]))
 
-        tau_sample = np.array([self.compute_exact_tau(
-            x_obs=sample_mat[kk, :, :], t0_val=t0_val, meshgrid=meshgrid) for kk in range(sample_mat.shape[0])])
+    def calculate_nuisance_parameters_over_grid(self, t0_grid, clf_odds, x_obs):
+        nuisance_param_grid = np.apply_along_axis(arr=t0_grid.reshape(-1, 1), axis=1,
+                                                  func1d=lambda row: minimize(
+            fun=partial(self._nuisance_parameter_func, x_obs=x_obs, target_params=row, clf_odds=clf_odds),
+            x0=np.array([0, 5, 1000]).reshape(1, 3)[:, self.nuisance_parameters_cols].reshape(-1,),
+            method='trust-constr', options={'verbose': 0}, bounds=self.bounds_opt).x)
 
-        return tau_sample
+        most_frequent_nuisance_param = mode_rows(nuisance_param_grid)
+        self.nuisance_param_val = np.array(most_frequent_nuisance_param)
+        t0_grid_nuisance = np.hstack((
+            t0_grid.reshape(-1, 1),
+            np.tile(self.nuisance_param_val, t0_grid.shape[0]).reshape(
+                t0_grid.shape[0], self.nuisance_param_val.shape[0])
+        ))
+        return t0_grid_nuisance
+
+    def _complete_theta_param_nuisance(self, t0_val):
+        return np.concatenate((np.array(t0_val).reshape(-1,), self.nuisance_param_val.reshape(-1,)))
 
     def sample_msnh_algo5(self, b_prime, sample_size):
         theta_mat = self.sample_param_values(sample_size=b_prime)
         assert theta_mat.shape == (b_prime, self.d)
 
+        # If we have nuisance parameters, we replace the values of those parameters with the parameter sampled
+        # with the nuisance parameters
+        if self.nuisance_flag:
+            theta_mat = np.apply_along_axis(arr=theta_mat, axis=1,
+                                            func1d=lambda row: self._complete_theta_param_nuisance(t0_val=row[0]))
+
         sample_mat = np.apply_along_axis(arr=theta_mat, axis=1,
                                          func1d=lambda row: self.sample_sim(sample_size=sample_size, true_param=row))
         return theta_mat, sample_mat
 
-    def make_grid_over_param_space(self, n_grid):
-        grid = tensor_4d_mesh(np.meshgrid(np.linspace(start=self.s_low, stop=self.s_high, num=n_grid),
-                                          np.linspace(start=self.r_low, stop=self.r_high, num=n_grid),
-                                          np.linspace(start=self.lambda_low, stop=self.lambda_high,
-                                                      num=n_grid),
-                                          np.linspace(start=self.b_low, stop=self.b_high, num=n_grid)))
-        # active_grid = self.select_active_parameters(grid)
-        return grid
-
-
-if __name__ == '__main__':
-
-    obj = InfernoToyLoader(benchmark=3)
-
-    sample_params = obj.sample_param_values(100)
-    samples = obj.generate_sample(sample_size=10)
-    x_mat_sampled = samples[:, 5:]
-    theta_mat_sampled = samples[:, :4]
-    print(theta_mat_sampled.shape, x_mat_sampled.shape)
-
-    print(obj.compute_exact_or(x_obs=x_mat_sampled, t0=theta_mat_sampled[0, :], t1=theta_mat_sampled[1, :]))
+# if __name__ == '__main__':
+#
+#     model_obj = InfernoToyLoader(benchmark=3)
+#
+#     sample = model_obj.generate_sample(sample_size=300)
+#     x_obs = model_obj.sample_sim(sample_size=10, true_param=model_obj.true_param)
+#     gen_sample_func = model_obj.generate_sample
+#     clf_odds = train_clf(sample_size=1000, clf_model=XGBRFClassifier(),
+#                          gen_function=gen_sample_func, clf_name='XGB', marginal=False, nn_square_root=True,
+#                          d=model_obj.d)
+#
+#     t0_grid = model_obj.calculate_nuisance_parameters_over_grid(
+#         t0_grid=model_obj.pred_grid, clf_odds=clf_odds, x_obs=x_obs)
+#
+#     theta_mat, sample_mat = model_obj.sample_msnh_algo5(b_prime=100, sample_size=10)
+#     print(theta_mat)
+#     print(sample_mat)
