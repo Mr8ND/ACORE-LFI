b34eded1cceede4cad31544b806b2e830a83a906
Mr8ND <niccolo.dalmasso@gmail.com>
2020-12-14 12:14:10 -0500

    Adapting b_prime_analysis for the INFERNO example.

--- acore/qr_algorithms/complete_list.py
+++ acore/qr_algorithms/complete_list.py
@@ -1,14 +1,14 @@
 classifier_cde_dict = {
     'xgb_d3_n100': ('xgb', {'max_depth': 3, 'n_estimators': 100}),
     'xgb_d3_n500': ('xgb', {'max_depth': 3, 'n_estimators': 500}),
-    'xgb_d3_n250': ('xgb', {'max_depth': 3, 'n_estimators': 250}),
-    'xgb_d5_n250': ('xgb', {'max_depth': 5, 'n_estimators': 250}),
+    # 'xgb_d3_n250': ('xgb', {'max_depth': 3, 'n_estimators': 250}),
+    # 'xgb_d5_n250': ('xgb', {'max_depth': 5, 'n_estimators': 250}),
     'xgb_d5_n100': ('xgb', {'max_depth': 5, 'n_estimators': 100}),
-    'xgb_d5_n500': ('xgb', {'max_depth': 5, 'n_estimators': 500}),
+    # 'xgb_d5_n500': ('xgb', {'max_depth': 5, 'n_estimators': 500}),
     'xgb_d10_n100': ('xgb', {'max_depth': 10, 'n_estimators': 100}),
-    'xgb_d10_n250': ('xgb', {'max_depth': 10, 'n_estimators': 100}),
-    'RF100': ('rf', {'n_estimators': 100}),
-    'RF250': ('rf', {'n_estimators': 250}),
+    # 'xgb_d10_n250': ('xgb', {'max_depth': 10, 'n_estimators': 100}),
+    # 'RF100': ('rf', {'n_estimators': 100}),
+    # 'RF250': ('rf', {'n_estimators': 250}),
     'RF500': ('rf', {'n_estimators': 500}),
     'lgb': ('lgb', {'num_leaves': 128, 'learning_rate': 0.1, 'n_estimators': 100,
                     'reg_sqrt': True, 'max_depth': 5}),
@@ -16,7 +16,7 @@ classifier_cde_dict = {
     'pytorch_a': ('pytorch', {'epochs': 500, 'batch_size': 50}, {'neur_shapes': (64, 32)}),
     'pytorch_b': ('pytorch', {'epochs': 500, 'batch_size': 50}, {'neur_shapes': (32, 16)}),
     'pytorch_3l': ('pytorch_3l', {'epochs': 500, 'batch_size': 50}, {'neur_shapes': (64, 64, 32)}),
-    'pytorch_3l_a': ('pytorch_3l', {'epochs': 500, 'batch_size': 50}, {'neur_shapes': (64, 32, 32)}),
-    'pytorch_3l_b': ('pytorch_3l', {'epochs': 500, 'batch_size': 50}, {'neur_shapes': (64, 32, 16)}),
-    'pytorch_3l_c': ('pytorch_3l', {'epochs': 500, 'batch_size': 50}, {'neur_shapes': (32, 32, 16)})
+    # 'pytorch_3l_a': ('pytorch_3l', {'epochs': 500, 'batch_size': 50}, {'neur_shapes': (64, 32, 32)}),
+    # 'pytorch_3l_b': ('pytorch_3l', {'epochs': 500, 'batch_size': 50}, {'neur_shapes': (64, 32, 16)}),
+    # 'pytorch_3l_c': ('pytorch_3l', {'epochs': 500, 'batch_size': 50}, {'neur_shapes': (32, 32, 16)})
 }
