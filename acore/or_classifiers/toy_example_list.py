36cb156fbec0c75b0bd9b8c62032f75e7d1f0ca5
Mr8ND <niccolo.dalmasso@gmail.com>
2020-12-31 14:15:02 -0500

    Removing QDA from p-value classifier as it is unstable in predicting probabilities.

--- acore/or_classifiers/toy_example_list.py
+++ acore/or_classifiers/toy_example_list.py
@@ -21,7 +21,6 @@ classifier_pvalue_dict = {
     'MLP': MLPClassifier(alpha=0, max_iter=25000),
     'MLP2': MLPClassifier((64, 32, 32), activation='relu', alpha=0, max_iter=25000),
     'XGBoost (d3, n500)': XGBClassifier(max_depth=3, n_estimators=500),
-    'QDA': QuadraticDiscriminantAnalysis(),
     'Log. Regr.': LogisticRegression(penalty='none', solver='saga', max_iter=10000)
 }
 
